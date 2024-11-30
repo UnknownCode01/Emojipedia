1b720ba1762af36898be8f2d87235ef6a8a73440
Brijit Adak <brijitadak000@gmail.com>
2024-12-01 04:05:37 +0530

    add

--- src/emojipedia.js
+++ src/emojipedia.js
@@ -4,7 +4,7 @@ let emojipedia = [];  // Initialize an empty array
 // Fetching emoji data from an API
 const fetchEmojipedia = async () => {
   try {
-    const response = await fetch('https://emoji-api.com/emojis?access_key=1c2318cd197da98a8e3fe6fa42600f05b1e23a05');  // Replace with your API URL
+    const response = await fetch('https://emoji-api.com/emojis?access_key=******');  // Replace with your API URL
     if (!response.ok) {
       throw new Error('Failed to fetch emoji data');
     }
